0703a41bbd5c7b1021f6478cb9e5c9584099c460
radhesharma2005 <mr.radheshyam2005@gmail.com>
2025-07-17 20:07:41 +0530

    Inputs in python

---
+++ inputinpython.py
@@ -0,0 +1,12 @@
+#syntax of taking input from user
+#input() is used to accept values from user 
+#input() result for input() is always a string 
+#int(input()) #int type input
+#float(input()) #float type input
+
+#examples
+# val=input("enter your age")
+# print(type(val),val)   #it is a string datatype by default as we know input() is always a string 
+
+# val=int(input("Enter the number:"))
+# print(type(val),val)  #it is integer type input 
