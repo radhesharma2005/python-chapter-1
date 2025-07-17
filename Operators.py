33d91b5f48b4d371651372f576fdbaf507d5a6fb
radhesharma2005 <mr.radheshyam2005@gmail.com>
2025-07-17 20:08:39 +0530

    operators in python

---
+++ Operators.py
@@ -0,0 +1,51 @@
+#Operators in python
+"""
+1)Arithmetic Operators:-(+,-,*,/,%,**)
+2)Relational/Comparision Operators:-(==,!=,>,<,>=,<=)
+3)Assignment operators:-(=,+=,-=,/=,*=,%=,**=)
+4)Logical Operators:-(not,and,or)
+"""
+
+#Arithmetic Operators
+# a=5
+# b=2
+# print(a+b) #Addition
+# print(a-b) #subtraction
+# print(a*b) #multiplication
+# print(a/b) #divide
+# print(a%b) #prints remainder (modulus)
+# print(a**b) #prints a^b , a raise to b
+
+#Relational Operators
+# a=60
+# b=78
+# print(a==b)   #False
+# print(a!=b)   #True
+# print(a>=b)   #False
+# print(a>b)    #False
+# print(a<=b)   #True
+# print(a<b)    #True
+
+#Assignment Operator
+#num=10
+ # num +=10
+ # print("num:",num)
+ # num-=10
+ # print("num:",num)
+ # num*=10
+ # print(num)
+ # num/=5
+ # print(num)
+ # num%=5
+ # print(num)
+ # num**=5
+ # print(num)
+
+ #Logical Operators
+# a=50
+# b=30
+# print(not a>b)
+# print(not a<b)
+# print("AND operator:",(a==b) and (a>b))
+# print("OR operator:",(a==b)or(a>b))
+
