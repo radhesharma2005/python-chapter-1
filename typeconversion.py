77eef415c546b7963a10d7fce014383a5d1746b1
radhesharma2005 <mr.radheshyam2005@gmail.com>
2025-07-17 20:09:54 +0530

    Conversions in python

---
+++ typeconversion.py
@@ -0,0 +1,17 @@
+#type conversion
+# a=2
+# b=2.15
+# sum=a+b
+# print(sum)
+
+#error
+# a="2"
+# b=2.14
+# sum=a+b
+# print(sum)
+
+#type casting
+# a=int("2")
+# b=2.15
+# print(type(a))
+# print(a+b)
