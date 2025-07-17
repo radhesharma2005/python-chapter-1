39c0418a1143234277810066cd67fcab5418c0cd
radhesharma2005 <mr.radheshyam2005@gmail.com>
2025-07-17 20:05:32 +0530

    Datatypes

---
+++ datatypes.py
@@ -0,0 +1,28 @@
+#Multiple line comments
+"""types datatype in python
+integer
+float
+boolean                  
+string
+none"""
+
+#strings can be written as              #single line commments
+name='radhe'
+name1="radhe"
+name2='''radhe'''
+print(name,name1,name2)
+print(type(name))
+
+#integers and float
+a=45.5
+b=-2
+c=a+b
+print(c)
+print(type(c))
+
+#using boolean and none datatype
+age=20
+old=True
+age=None
+print(type(age))
+print(type(old))
