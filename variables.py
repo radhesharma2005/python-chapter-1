4a6596d5a60383f010de664fcb6e56e1f0162ad4
radhesharma2005 <mr.radheshyam2005@gmail.com>
2025-07-17 20:11:03 +0530

    variables in python

---
+++ variables.py
@@ -0,0 +1,19 @@
+#defining variables
+name="Radheshyam" #string
+age=20
+price=45.77
+
+age2=age #storing data of age in age2
+
+
+#printing output using variables
+print(name)
+print(age2)
+print(price)
+
+
+#find type of datatype in python using function type()
+
+print(type(name))
+print(type(age))
+print(type(price))
