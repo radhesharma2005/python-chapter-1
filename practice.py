bb50cc8bdc9854972d9b2898358564130c43b33e
radhesharma2005 <mr.radheshyam2005@gmail.com>
2025-07-17 20:11:51 +0530

    Questions in python

---
+++ practice.py
@@ -0,0 +1,20 @@
+#write a program taking two input and sum it
+"""num1=int(input("Enter num1:"))
+num2=int(input("Enter num2:"))
+sum=num1+num2
+print("the sum two number is:-",sum)"""
+
+#WAP area of square
+""" side=float(input("Enter the side of square:-"))
+# print("area:-",side*side)"""
+
+#WAP with floating point numbers and find average of it
+"""first=float(input("Enter the first number:-"))
+second=float(input("Enter the second number:-"))
+average=(first+second)/2
+print("average=",average)"""
+
+#WAP to input 2 int numbers a and b,print True if a is greater than or equal to b .if not print false
+"""a=int(input("Enter the first number:-"))
+b=int(input("Enter the second number:-"))
+print(a>=b)"""
